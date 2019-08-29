616de8a4514a5b794bd92348d9477c06a602e1c4
Iuri Pereira <iuricmp@gmail.com>
2019-08-29 21:32:51 +0100

    Publish
     - @react-logistic-components/driver-avatar@0.0.7-alpha.0
     - @react-logistic-components/vehicle-marker-dialog@0.0.5-alpha.0

--- packages/VehicleMarkerDialog/src/VehicleMarkerDialog.js
+++ packages/VehicleMarkerDialog/src/VehicleMarkerDialog.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-function VehicleMarkerDialog() {
-  return <h1>VehicleMarkerDialog 2</h1>
-}
+const VehicleMarkerDialog = React.forwardRef((props, ref) => (
+  <button ref={ref} className="FancyButton">
+    {props.children}
+  </button>
+));
 
 export default VehicleMarkerDialog
