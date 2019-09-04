45d2c090455f4da4aebbefa35db2a693bcb519f7
Iuri Pereira <iuricmp@gmail.com>
2019-09-04 05:56:50 +0100

    chore(eslint and prettier): esLint and Prettier setup

---
+++ .eslintrc.js
@@ -0,0 +1,76 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb', 'prettier', 'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react', 'react-hooks', 'prettier'
+  ],
+  rules: {
+    'consistent-this': ['error', 'self'],
+    'linebreak-style': 'off', // Doesn't play nicely with Windows
+    'no-alert': 'error',
+    // Strict, airbnb is using warn
+    'no-console': 'error',
+    'no-constant-condition': 'error',
+    // Airbnb use error
+    'no-param-reassign': 'off',
+    'no-prototype-builtins': 'off',
+    'prefer-arrow-callback': ['error', { allowNamedFunctions: true }],
+    'prefer-destructuring': 'off', // Destructuring harm grep potential.
+
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/label-has-for': 'off', // deprecated
+    'jsx-a11y/no-autofocus': 'off', // We are a library, people do what they want.
+
+    // This rule is great for raising people awareness of what a key is and how it works.
+    'react/no-array-index-key': 'off',
+    'react/destructuring-assignment': 'off',
+    // It's buggy
+    'react/forbid-prop-types': 'off',
+    'react/jsx-curly-brace-presence': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }], // airbnb is using .jsx
+    'react/jsx-handler-names': [
+      'error',
+      {
+        // airbnb is disabling this rule
+        eventHandlerPrefix: 'handle',
+        eventHandlerPropPrefix: 'on',
+      },
+    ],
+    'react/no-danger': 'error',
+    // Strict, airbnb is using off
+    'react/no-direct-mutation-state': 'error',
+    'react/no-find-dom-node': 'off',
+    'react/no-multi-comp': 'off',
+    'react/require-default-props': 'off',
+    'react/sort-prop-types': 'error',
+
+    'import/no-extraneous-dependencies': 'off', // It would be better to enable this rule.
+    'import/namespace': ['error', { allowComputed: true }],
+    'import/order': [
+      'error',
+      {
+        groups: [['index', 'sibling', 'parent', 'internal', 'external', 'builtin']],
+        'newlines-between': 'never',
+      },
+    ],
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+  },
+};
